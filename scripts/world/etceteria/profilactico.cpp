ce32d662f98401747840cd401c7dc0006cc5523a
Luis Isea <19-10175@usb.ve>
2023-11-02 10:49:02 -0400

    Listos diálogos de etceteria

---
+++ scripts/world/etceteria/profilactico.cpp
@@ -0,0 +1,22 @@
+#!/bin/bash
+### Script de hablar con profilactico ###
+
+### Variables ###
+FILE1="grub.old"
+FILE2="prolifactico.cpp"
+FILE3="profile"
+
+### Dialogo ###
+clear
+echo -e "$ ${BLUE}$PLAYER${NC}: ¿Soy yo o hace frio aca?" | pv -qL100
+echo -e "$ ${GREEN}$FILE1${NC}: Estamos llegando cada vez mas profundo en la etceteria." | pv -qL100
+echo -e "$ ${BLUE}$PLAYER${NC}: Ya veo, ¿y ellos? ¿quienes son? Parecen sospechosos." | pv -qL100
+echo -e "$ ${GREEN}$FILE1${NC}: La verdad se me hacen conocidos, parecen buenos. Mira, estan hablando entre ellos." | pv -qL100
+echo -e "$ ${BLUE}$PLAYER${NC}: Me acercare. Hola, ¿que tal todo?" | pv -qL100
+echo -e "$ ${YELLOW}$FILE3${NC}: Aqui estamos, eliminando variables del sistema porque el usuario tiene demasiadas, eso me da ansiedad." | pv -qL100
+echo -e "$ ${BLUE}$PLAYER${NC}: Mmm ya veo, ¿y tu? ¿que haces?" | pv -qL100
+echo -e "$ ${PURPLE}$FILE2${NC}: Aqui, esperando que caiga mi primera chamba." | pv -qL100
+echo -e "$ ${GREEN}$FILE1${NC}: Te lo dije, no son malos, ya podemos dejarlos en paz." | pv -qL100
+
+### Fin del dialogo ###
+press_enter
